4dce4dd86517e360026c949c235b9df8b44b6069
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-01-18 11:56:54 +0200

    add hm2

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,30 +10,14 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
+           
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
 
-            while (true)
-            {
-                Console.WriteLine("Калькулятор систем числення");
-                Console.WriteLine("1. Двійкова");
-                Console.WriteLine("2. Вісімкова");
-                Console.WriteLine("3. Шістнадцяткова");
-                Console.WriteLine("4. Вихід");
-                Console.Write("Виберіть систему числення: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
-
-                if (choice == 4)
-                {
-                    break;
-                }
-
-                Console.Write("Введіть число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-                Convertor convertor = new Convertor();
-                convertor.SetAndDispay(num, choice);
-                Console.WriteLine();
+                Console.WriteLine("Enter a number in words: ");
+                var word = Console.ReadLine();
+                int num = ConverFromWordToNumber.Convert(word);
+                Console.WriteLine(num);
             }
         }
     }
-}
